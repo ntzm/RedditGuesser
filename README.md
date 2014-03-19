e4db906a2b2f6e3af6c728365b26901d4247e0d3
NatZim <nat@natzim.com>
2014-03-19 17:40:06 +0000

    v0.1.2
    - Changed jQuery version to support older browsers
    - Changed cog icon to info icon
    - Changed some classes into ids for speed purposes
    - Changed "limit" to "results per subreddit"
    - Replaced individual subreddit variables with array to allow for
    eventual expandability
    - Added javascript annotation
    - Added basic error reporting
    - Removed button colouring to allow for eventual expandability
    - Removed that damned JSHint code

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ Type in two subreddits, and decide which title belongs to which subreddit!
 
 ### In the near future:
 
+- Have an equal number of results per subreddit (will take longer, so will introduce a loading bar of some description)
 - Make a branch which uses PHP to call the reddit API, so people at work or school where reddit is blocked can still use it
 - Improve error reporting
 - Add more options
 - Make it harder to cheat
 - Have the option to add more subreddits for an extra challenge
+- Clean up bootstrap CSS that I stole (make sure to check responsiveness)
 
 ### In the distant future:
 
@@ -20,7 +22,20 @@ Type in two subreddits, and decide which title belongs to which subreddit!
 
 ## Changelog
 
+### v0.1.2
+
+- Changed jQuery version to support older browsers
+- Changed cog icon to info icon
+- Changed some classes into ids for speed purposes
+- Changed "limit" to "results per subreddit"
+- Replaced individual subreddit variables with array to allow for eventual expandability
+- Added javascript annotation
+- Added basic error reporting
+- Removed button colouring to allow for eventual expandability
+
 ### v0.1.1
+
+- (minor fix) Forced all inputs and solutions to be lower case
 
 - Improved reddit API call by merging the two calls
 - Added an endgame tab which shows your score
--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-/*global titles:true, sr1:true, sr2:true, i:true, score:true, solutions:true, limit:true, order:true, id:true, row:true, answer:true, answered:true */
+// Called every time the score increases
 function checkScore() {
+  // If number of guesses is equal to the number of solutions
   if (answered === solutions.length) {
+    // Display score
     $("#titlecontainer").append("<div class='titlewrap'><h2>Congratulations!<h2><h3>Score: " + score + "/" + answered + "</h3></div>");
   }
 }
+
 $(document).ready(function () {
-  $(".progress").hide();
-  $("form").submit(function (e) {
+  // When the main form is submitted
+  $("#form-main").submit(function (e) {
+    // Stops the form from submitting
     e.preventDefault();
-    sr1 = $("#sr1").val().toLowerCase();
-    sr2 = $("#sr2").val().toLowerCase();
-    if (sr1 === "" || sr2 === "") {
+    // Defining subreddit array and validating inputs
+    subs = [];
+    subs.push($("#sr1").val().toLowerCase());
+    subs.push($("#sr2").val().toLowerCase());
+    if (subs[0] === "" || subs[1] === "") {
+      // If the arrays items are blank, needs to be improved to check all the items in the array
       $("#message").html("You do realise you have to actually type something in, right?");
-    } else if (sr1 === sr2) {
+    } else if (subs[0] === subs[1]) {
+      // If subreddits are the same, needs to be improved to check all the items in the array
       $("#message").html("Stop trying to break me, you can't use the same two subreddits");
     } else {
+      // If everything is all good - reset all the variables and stuff to default
       $("#message").html("");
       $("#titlecontainer").html("");
       score = 0;
@@ -22,38 +31,66 @@ $(document).ready(function () {
       titles = [];
       solutions = [];
       limit = $("#limit").val();
-      if (parseInt(limit, 10) < 1 || limit === "") {
-        limit = "10";
+      // Limit validation, if limit is below 1, if limit is blank or if limit contains letters, set the limit to 5
+      if (parseInt(limit, 10) < 1 || limit === "" || /[^0-9]/.test(limit)) {
+        limit = "5";
       }
       order = $("#order").val();
-      $.getJSON("http://www.reddit.com/r/" + sr1 + "+" + sr2 + "/" + order + ".json?jsonp=?&limit=" + limit, function (combined) {
+      // Get reddit data for the selected subreddits, needs to be improved so each subreddit has its own call, to make the numbers of results equal
+      $.getJSON("http://www.reddit.com/r/" + subs[0] + "+" + subs[1] + "/" + order + ".json?jsonp=?&limit=" + limit, function (combined) {
+        // For each of the results push the title and solution to seperate arrays, needs to be improved to a 2d array so they can be scrambled
         $.each(combined.data.children, function (i, data) {
           titles.push(data.data.title);
           lastsolution = data.data.subreddit;
+          // Turns the subreddits lower case so it validates with the user input
           solutions.push(lastsolution.toLowerCase());
         });
-      }).done(function () {
-        for (i = 0; i < titles.length; i++) {
-          $("#titlecontainer").append("<div class='titlewrap' id='titlewrap" + i + "'><h3 class='title' id='title" + i + "'>" + titles[i] + "</h3><p><button class='btn btn-success btn-choice' id='" + sr1 + i + "'>" + sr1 + "</button><button class='btn btn-warning btn-choice' id='" + sr2 + i + "'>" + sr2 + "</button></p></div>");
-        }
-      });
+      })
+        // If the call is successful
+        .done(function () {
+          // Displays a title and buttons for each result, needs to be improved to allow for infinite amount of subreddits
+          for (i = 0; i < titles.length; i++) {
+            $("#titlecontainer").append("<div class='titlewrap' id='titlewrap" + i + "'><h3 class='title' id='title" + i + "'>" + titles[i] + "</h3><p><button class='btn btn-choice' id='" + subs[0] + i + "'>" + subs[0] + "</button><button class='btn btn-choice' id='" + subs[1] + i + "'>" + subs[1] + "</button></p></div>");
+          }
+        })
+        // If something bad happens, needs to be improved to explain to the user what is wrong
+        .fail(function () {
+          $("#message").html("Subreddit does not exist or access to reddit is denied");
+        });
     }
   });
+
+  // Open the settings
   $("#settings").click(function () {
     $(".settings").slideToggle(600);
   });
+
+  // If the settings form is submitted, do nothing
+  $("#settingsform").submit(function (e) {
+    e.preventDefault();
+  });
+
+  // If a choice button is clicked
   $(document).on("click", ".btn-choice", function () {
+    // Defining variables
     id = $(this).attr("id");
+    // Splits the id into two parts
     row = id.replace(/\D/g, '');
     answer = id.replace(/\d+/g, '');
-    $("#" + sr1 + row + ",#" + sr2 + row).attr("disabled", true);
+    // Disables the buttons
+    $("#" + subs[0] + row + ",#" + subs[1] + row).attr("disabled", true);
+    // If the answer is correct
     if (solutions[row] === answer) {
+      // Make the background green and slide up
       $("#titlewrap" + row).css("background", "#47a447").slideUp(600);
+      // Add to answered and score
       answered++;
       score++;
       checkScore();
     } else {
+      // Make the background red and slide up
       $("#titlewrap" + row).css("background", "#d2322d").slideUp(600);
+      // Add to answered
       answered++;
       checkScore();
     }
